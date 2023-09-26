61d21089779e89159ce84f2856db319ec897101a
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-09-26 13:05:59 +0500

    Update phonebook_code.py

--- phonebook_code.py
+++ phonebook_code.py
@@ -1,102 +1,56 @@
 from pprint import pprint
+## Читаем адресную книгу в формате CSV в список contacts_list:
 import csv
-import io
 import re
-import os
-import operator
-import itertools
-
-with io.open("phonebook_raw.csv", encoding='utf-8') as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
-
-
-## 1. Выполните пункты 1-3 задания.
-def csv_to_dict(file_name):
-  contact_dict = []
-  with open(file_name, encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',')
-    contact_list = list(reader)
-
-    keys = contact_list[0]
-    values = contact_list[1:]
-    for name, infos in enumerate(values):
-      contact_dict.append({})
-      for key, info in zip(keys, infos):
-        contact_dict[name].update({key: info})
-    return contact_dict
-
-
-def delete_dup(in_file):
-    contact_dict = csv_to_dict(in_file)
-    for names in contact_dict:
-      splitted = names['lastname'].split(' ')
-      if len(splitted) > 1:
-        names['lastname'] = splitted[0]
-        names['firstname'] = splitted[1]
-        if len(splitted) > 2:
-          names['surname'] = splitted[2]
-      
-      splitted = names['firstname'].split(' ')
-      
-      if len(splitted) > 1:
-        names['firstname'] = splitted[0]
-        names['surname'] = splitted[1]
-    
-    return contact_dict
-
-
-def fix_phones(in_file, out_file):
-    with open(in_file, encoding="utf8") as f:
-        text = f.read()
-
-    phone_pattern = r'(\+7|8)?\s*\(?(\d{3})\)?[\s*-]?(\d{3})[\s*-]?(\d{2})[\s*-]?(\d{2})(\s*)\(?(доб\.?)?\s*(\d*)?\)?'
-    fixed_phone = re.sub(phone_pattern, r'+7(\2)\3-\4-\5\6\7\8', text)
-    with open(out_file, 'w+', encoding="utf8") as f:
-        text = f.write(fixed_phone)
+import io 
 
 
 
-
-def merge_names(contacts):
-    all_keys = set(contacts[0].keys())
-    group_list = ['firstname', 'lastname']
-    group = operator.itemgetter(*group_list)
-    cols = operator.itemgetter(*(all_keys ^ set(group_list)))
-    contacts.sort(key=group)
-    grouped = itertools.groupby(contacts, group)
-
-    merge_data = []
-    for (firstname, lastname), g in grouped:
-        merge_data.append({'lastname': lastname, 'firstname': firstname})
-        for gr in g:
-            d1 = merge_data[-1]
-            for k, v in gr.items():
-                if k not in d1 or d1[k] == '':
-                    d1[k] = v
-
-    return merge_data
+def fix_contacts(contacts_list):
+  for item in contacts_list:
+    contact_fullname = ' '.join(item[:3]).split('  ')
+    print(contact_fullname)
 
 
+complete = {}
+def delete_dup(contacts_list):
+    for contact in contacts_list:
+      information = contact[3:]
+      names = contact[0]
+      if names not in complete:
+        complete.update({names: information})
+      else:
+        for i, value in enumerate(complete[names]):
+          if not value:
+            complete[names][i] = information[i]
+    print(complete)
 
 
-def dicts_to_file(file_name, dicts):
-  keys = list(dicts[0].keys())
-  with open(file_name, 'w', encoding='utf-8') as f:
-    datawriter = csv.writer(f, delimiter=',')
-    datawriter.writerows(keys)
-    for d in dicts:
-      datawriter.writerow(d.values())
-
-
-
-
+def fix_phones(contacts_list):
+  phone_pattern = re.compile(r'(\+7|8)?\s*\(?(\d{3})\)?\s*\D?(\d{3})[-\s+]?(\d{2})-?(\d{2})((\s)?\(?(доб.)?\s?(\d+)\)?)?')
+  phone_substitution = r'+7-(\2) \3-\4-\5'
+  
+  for phones in contacts_list:
+    broken = phones[5]
+    if broken == 'phone':
+      phones.remove('phone')
+    else:
+      fixed_phone = re.sub(phone_pattern, phone_substitution, broken).split()
+      print(fixed_phone)
 
 
 
 if __name__ == '__main__':
-   fix_phones(in_file = 'phonebook_raw.csv', out_file='fixed_phone.csv')
-   fixed_names = delete_dup(in_file = 'fixed_phone.csv')
-   os.remove('fixed_phone.csv')
-   merged_data = merge_names(fixed_names)
-   dicts_to_file("phonebook.csv", merged_data)
+  with io.open("phonebook_raw.csv", encoding='utf-8') as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+    contact_list_updated = []
+    # fix_contacts(contacts_list)
+    # fix_duplicate(contacts_list)
+    # fix_phones(contacts_list)
+    delete_dup(contacts_list)
+  
+  # with open("phonebook.csv", "w", encoding='utf-8') as out_file:
+  #   datawriter = csv.writer(out_file, delimiter=',')
+  #   datawriter.writerows(contact_list_updated)
+  # pprint(contact_list_updated)
