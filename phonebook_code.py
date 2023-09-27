c6840e0ac3bd79be6eeadf34022d76dfb13810aa
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-09-27 18:29:59 +0500

    Update phonebook_code.py

--- phonebook_code.py
+++ phonebook_code.py
@@ -12,18 +12,45 @@ def fix_contacts(contacts_list):
     print(contact_fullname)
 
 
+
+def fix_duplicate(contacts_list):
+  fixed_contacts = []
+  first_names = []
+  surnames = []
+  for item in contacts_list:
+    lastnames = item[0]
+    separated_last = lastnames.split()
+    if len(separated_last) > 1:
+      first_names.append(separated_last[1])
+      separated_last.pop(1)
+      if len(separated_last) > 1:
+        surnames.append(separated_last[1])
+        separated_last.pop(1)
+    if separated_last[0] == 'lastname':
+      separated_last.pop(0)
+    for i in separated_last:
+      fixed_contacts.append(i)
+  return fixed_contacts
+
+
+    
 complete = {}
 def delete_dup(contacts_list):
     for contact in contacts_list:
-      information = contact[3:]
-      names = contact[0]
+      information = contact[0:]
+      names = fix_duplicate(contacts_list)
+      if names not in complete:
+        complete.update({names:information})
       if names not in complete:
         complete.update({names: information})
+        print(information)
       else:
-        for i, value in enumerate(complete[names]):
+        for i, value in enumerate(complete[keys]):
           if not value:
-            complete[names][i] = information[i]
+            complete[keys][i] = information[i]
     print(complete)
+
+
 
 
 def fix_phones(contacts_list):
@@ -37,6 +64,11 @@ def fix_phones(contacts_list):
     else:
       fixed_phone = re.sub(phone_pattern, phone_substitution, broken).split()
       print(fixed_phone)
+
+
+
+
+
 
 
 
