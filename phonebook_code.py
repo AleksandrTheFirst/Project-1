6b892d78f175f5ec14bdcf34198c351707e6b1cb
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-09-28 17:27:51 +0500

    Update phonebook_code.py

--- phonebook_code.py
+++ phonebook_code.py
@@ -9,28 +9,29 @@ import io
 def fix_contacts(contacts_list):
   for item in contacts_list:
     contact_fullname = ' '.join(item[:3]).split('  ')
-    print(contact_fullname)
+    return contact_fullname
 
 
-
-def fix_duplicate(contacts_list):
-  fixed_contacts = []
-  first_names = []
-  surnames = []
-  for item in contacts_list:
-    lastnames = item[0]
-    separated_last = lastnames.split()
-    if len(separated_last) > 1:
-      first_names.append(separated_last[1])
-      separated_last.pop(1)
-      if len(separated_last) > 1:
-        surnames.append(separated_last[1])
-        separated_last.pop(1)
-    if separated_last[0] == 'lastname':
-      separated_last.pop(0)
-    for i in separated_last:
-      fixed_contacts.append(i)
-  return fixed_contacts
+# full_fio = {}
+# def fix_duplicate(contacts_list):
+#   fixed_contacts = []
+#   first_names = []
+#   surnames = []
+#   for item in contacts_list:
+#     lastnames = item[0]
+#     separated_last = lastnames.split()
+#     if len(separated_last) > 1:
+#       first_names.append(separated_last[1])
+#       separated_last.pop(1)
+#       if len(separated_last) > 1:
+#         surnames.append(separated_last[1])
+#         separated_last.pop(1)
+#     if separated_last[0] == 'lastname':
+#       separated_last.pop(0)
+#     for i in separated_last:
+#       fixed_contacts.append(i)
+#   full_fio.update({'lastname': fixed_contacts, 'firstname': first_names, 'surname': surnames})
+#   print(full_fio)
 
 
     
@@ -38,18 +39,29 @@ complete = {}
 def delete_dup(contacts_list):
     for contact in contacts_list:
       information = contact[0:]
-      names = fix_duplicate(contacts_list)
+      fio = ' '.join(contact[:3]).split()
+      contact[:len(fio)] = fio
+      names = contact[0]
       if names not in complete:
         complete.update({names:information})
       if names not in complete:
         complete.update({names: information})
         print(information)
       else:
-        for i, value in enumerate(complete[keys]):
+        for i, value in enumerate(complete[names]):
           if not value:
-            complete[keys][i] = information[i]
-    print(complete)
+            complete[names][i] = information[i]
+    for i, value in complete.items():
+      contact_list_updated.append(value)
+    return contact_list_updated
+    # for contacted in contact_list_updated:
+    #   splitted = ' '.join(contacted[:3]).split()
+    #   contact[:len(splitted)] = splitted
+    #   contact_list_updated.append(splitted)
+    # return splitted
 
+
+        
 
 
 
@@ -63,11 +75,7 @@ def fix_phones(contacts_list):
       phones.remove('phone')
     else:
       fixed_phone = re.sub(phone_pattern, phone_substitution, broken).split()
-      print(fixed_phone)
-
-
-
-
+      return fixed_phone
 
 
 
@@ -77,12 +85,11 @@ if __name__ == '__main__':
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
     contact_list_updated = []
-    # fix_contacts(contacts_list)
-    # fix_duplicate(contacts_list)
-    # fix_phones(contacts_list)
+    fix_contacts(contacts_list)
+    fix_phones(contacts_list)
     delete_dup(contacts_list)
   
-  # with open("phonebook.csv", "w", encoding='utf-8') as out_file:
-  #   datawriter = csv.writer(out_file, delimiter=',')
-  #   datawriter.writerows(contact_list_updated)
-  # pprint(contact_list_updated)
+  with open("phonebook.csv", "w", encoding='utf-8') as out_file:
+    datawriter = csv.writer(out_file, delimiter=',')
+    datawriter.writerows(contact_list_updated)
+  pprint(contact_list_updated)
